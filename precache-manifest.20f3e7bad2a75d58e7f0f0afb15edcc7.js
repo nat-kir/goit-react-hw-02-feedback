d4e487432858c240d8691eef56dd28bd4210f2c5
Kiriunina Natalia <kiriunina.natalia@gmail.com>
2020-09-11 21:56:06 +0300

    Updates

---
+++ precache-manifest.20f3e7bad2a75d58e7f0f0afb15edcc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba486f9feeefccdfade065d000f9ef6a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "4c3021288ac1aa870235",
+    "url": "/goit-react-hw-02-feedback/static/css/2.972ec59c.chunk.css"
+  },
+  {
+    "revision": "2eb05f09c9dad0bfdbb7",
+    "url": "/goit-react-hw-02-feedback/static/css/main.082e06cd.chunk.css"
+  },
+  {
+    "revision": "4c3021288ac1aa870235",
+    "url": "/goit-react-hw-02-feedback/static/js/2.44e95ba4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.44e95ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb05f09c9dad0bfdbb7",
+    "url": "/goit-react-hw-02-feedback/static/js/main.9977f477.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
