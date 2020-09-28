dc62090df23dacee5303c2061b400b7751629e71
Kiriunina Natalia <kiriunina.natalia@gmail.com>
2020-09-28 21:26:25 +0300

    2nd submition feedback

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Feedback.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
@@ -15,5 +16,14 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => (
     ))}
   </div>
 );
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      option: PropTypes.string.isRequired,
+    }).isRequired,
+  ),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
 export default FeedbackOptions;
